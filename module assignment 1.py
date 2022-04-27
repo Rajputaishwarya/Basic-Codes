4175006db714f5e240c7cfb9d8cbacb721d6c1e8
Rajputaishwarya <102976991+Rajputaishwarya@users.noreply.github.com>
2022-04-27 10:37:44 +0530

    Add files via upload

---
+++ module assignment 1.py
@@ -0,0 +1,28 @@
+# print a string
+print('hello python')
+# store a variable and print it
+a='Aishwarya'
+print(a)
+# sum of two value
+x=90
+y=55
+z=x+y
+print(z)
+# division of two value
+c=x/y
+print(c)
+# multiple value of variable
+v=(1,2,3,4,5)
+print(v)
+# check type of variable
+print(type(v))
+d=int(input('enter thr value of D: '))
+f=int(input('enter the value of F: '))
+if d==f:
+    print('true')
+else:
+    print('false')
+# print value of user define
+s=(int(input("user input: ")))
+print(s)
+
